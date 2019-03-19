1073bb61a748f4946c52dc7154bf1662c239ac17
naoyuki29 <58511848+naoyuki29@users.noreply.github.com>
2019-03-19 15:50:20 +0900

    mail情報をconfigに追加

--- src/utils/site_config.js
+++ src/utils/site_config.js
@@ -2,8 +2,8 @@ const config = {
     siteTitle: "HogeNote", // Site title.
     siteDescription: "Construction of new note", // Website description used for RSS feeds/meta description tag.
     dateFormat: "YY/MM/DD", // Date format for display.
-    userName: "aka", // Username to display in the author segment.
-    userEmail: "aka.yuki2924@gmail.com", // Email used for RSS feed's author segment
+    userName: "naoyuki-u", // Username to display in the author segment.
+    userEmail: "naoyuki.gatsby@gmail.com", // Email used for RSS feed's author segment
     copyright: "Copyright Ã‚Â© 2019. N.Uesugi", // Copyright string for the footer of the website and RSS feed.
 
     postNumberPerPage: "5",
