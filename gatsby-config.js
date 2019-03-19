63d1a6a8ff8c5ab0f1b1f44bedec22ee2e6a0d14
naoyuki-u <n.ue7083@gmail.com>
2019-03-19 13:25:49 +0900

    サイト情報を追記

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: ``,
+    author: `@naoyuki-u`,
+    postNumberPerPage: `5`
   },
   plugins: [
     "gatsby-plugin-sass",
