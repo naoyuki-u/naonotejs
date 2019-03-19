0ee4ffc4a00569dc059c3a6ae00a750f0fa294ca
naoyuki-u <n.ue7083@gmail.com>
2019-03-19 13:24:25 +0900

    #6 ページングの追加

--- src/components/postlist/postlist.jsx
+++ src/components/postlist/postlist.jsx
@@ -1,20 +1,39 @@
 import React, { Component } from "react"
-import { graphql, Link } from "gatsby"
-import { Card, Icon, Tag } from "antd"
+import { graphql, StaticQuery } from "gatsby"
+import { Pagination } from "antd"
 import PostCard from "../postcard"
 
 class PostList extends Component {
   render() {
-    const { edges } = this.props;
+    const { edges, index } = this.props;
+    const total_post = edges.length;
     return (
-      <div>
-        {
-          edges.map(({ node }) => (
-            <PostCard node={node}>
-            </PostCard>
-          ))
-        }
-      </div>
+      <StaticQuery
+        query={graphql`
+          query{
+            site{
+              siteMetadata{
+                postNumberPerPage
+              }
+            }
+          }
+        `}
+        render={data => {
+          const total_page = Math.ceil(
+                        total_post / data.site.siteMetadata.postNumberPerPage);
+          return (
+            <div>
+              {
+                edges.map(({ node }) => (
+                  <PostCard node={node}>
+                  </PostCard>
+                ))
+              }
+              <Pagination total={total_page * 10} defaultCurrent={Number(index)} />
+            </div>
+          )
+        }}
+      />
     )
   }
 }
