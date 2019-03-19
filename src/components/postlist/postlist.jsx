720cee63090730249139564976a7119840ae3e02
naoyuki29 <58511848+naoyuki29@users.noreply.github.com>
2019-03-19 13:34:05 +0900

    #6 1ページあたりの表示記事を制限

--- src/components/postlist/postlist.jsx
+++ src/components/postlist/postlist.jsx
@@ -7,6 +7,7 @@ class PostList extends Component {
   render() {
     const { edges, index } = this.props;
     const total_post = edges.length;
+    const page_index = Number(index);
     return (
       <StaticQuery
         query={graphql`
@@ -19,17 +20,22 @@ class PostList extends Component {
           }
         `}
         render={data => {
-          const total_page = Math.ceil(
-                        total_post / data.site.siteMetadata.postNumberPerPage);
+          const num_per_page = data.site.siteMetadata.postNumberPerPage;
+          const total_page = Math.ceil(total_post / num_per_page);
+          const post_begin = (page_index - 1) * num_per_page;
+          var post_end = page_index * num_per_page;
+          if (post_end > total_post){
+            post_end = total_post;
+          }
           return (
             <div>
               {
-                edges.map(({ node }) => (
+                edges.slice(post_begin, post_end).map(({ node }) => (
                   <PostCard node={node}>
                   </PostCard>
                 ))
               }
-              <Pagination total={total_page * 10} defaultCurrent={Number(index)} />
+              <Pagination total={total_page * 10} defaultCurrent={page_index} />
             </div>
           )
         }}
