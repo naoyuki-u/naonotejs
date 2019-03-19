78d932f8c080391f8ca8b127656b1b7575c2f16b
naoyuki29 <58511848+naoyuki29@users.noreply.github.com>
2019-03-19 18:24:22 +0900

    #8style 文字コードがいつの間にか変わってた..

--- src/components/taglist/taglist.jsx
+++ src/components/taglist/taglist.jsx
@@ -23,7 +23,7 @@ class TagList extends Component {
     return(
       <div>
         <br/>
-        <h3>タグ</h3>
+        <h3>ã‚¿ã‚°</h3>
         <p></p>
         <div>
           {tagArray.map((tag) => (
