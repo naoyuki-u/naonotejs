4c667734a335664deece350c72e82772621bf3ca
naoyuki-u <n.ue7083@gmail.com>
2019-04-11 10:19:41 +0900

    temporary

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,16 +1,26 @@
 import React from "react"
 import {graphql} from "gatsby"
 import BlogLayout from "../components/layout_blog"
+import SEO from "../components/seo"
 
 export default class BlogPostTemplate extends React.Component{
   render() {
     const {data} = this.props;
     const post = data.markdownRemark;
+    const { slug } = this.props.pageContext;
+
     return (
-      <BlogLayout
-        title={post.frontmatter.title}
-        body={post.html}
-      />
+      <div>
+        <SEO
+          postNode={post}
+          isPost={true}
+          slug={slug} />
+
+        <BlogLayout
+          title={post.frontmatter.title}
+          body={post.html}
+        />
+      </div>
     )
   }
 }
--- src/utils/site_config.js
+++ src/utils/site_config.js
@@ -1,10 +1,14 @@
 const config = {
     siteTitle: "HogeNote", // Site title.
-    siteDescription: "Construction of new note", // Website description used for RSS feeds/meta description tag.
+    siteDescription: "My blog powered by gatsbyjs.", // Website description used for RSS feeds/meta description tag.
     dateFormat: "YY/MM/DD", // Date format for display.
     userName: "naoyuki-u", // Username to display in the author segment.
     userEmail: "naoyuki.gatsby@gmail.com", // Email used for RSS feed's author segment
-    copyright: "Copyright Ã‚Â© 2019. N.Uesugi", // Copyright string for the footer of the website and RSS feed.
+    copyright: "Copyright (c) 2019. N.Uesugi", // Copyright string for the footer of the website and RSS feed.
+
+    siteUrl: "/",
+    pathPrefix: "/",
+    siteLogo: "/logos/gatsby-icon.png",
 
     postNumberPerPage: "5",
     pagesRoot: "pages-",
