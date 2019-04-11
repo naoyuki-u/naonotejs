cd1b4829be1c48ce2f297fb1ed6cc76fe8955e5f
naoyuki29 <58511848+naoyuki29@users.noreply.github.com>
2019-04-11 10:19:41 +0900

    temporary

---
+++ src/components/seo/seo.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from "react"
+import { Helmet } from "react-helmet"
+
+const config = require('../../utils/site_config')
+
+class SEO extends Component {
+  render() {
+    const { postNode, isPost, slug } = this.props;
+    let title;
+    let description;
+    let image;
+    let postURL;
+    if (isPost) {
+      const postMeta = postNode.frontmatter;
+      ({ title } = postMeta);
+      description = postMeta.description
+        ? postMeta.description
+        : postNode.excerpt;
+      image = postMeta.cover;
+      postURL = urljoin(config.siteUrl, config.pathPrefix, slug);
+    } else {
+      title = config.siteTitle;
+      description = config.siteDescription;
+      image = config.siteLogo;
+    }
+    image = urljoin(config.siteUrl, config.pathPrefix, image);
+    const blogURL = urljoin(config.siteUrl, config.pathPrefix);
+    const schemaOrgJSONLD = [
+      {
+        "@context": "http://schema.org",
+        "@type": "WebSite",
+        url: blogURL,
+        name: title,
+        alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
+      }
+    ];
+    if (isPost) {
+      schemaOrgJSONLD.push([
+        {
+          "@context": "http://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              item: {
+                "@id": postURL,
+                name: title,
+                image
+              }
+            }
+          ]
+        },
+        {
+          "@context": "http://schema.org",
+          "@type": "BlogPosting",
+          url: blogURL,
+          name: title,
+          alternateName: config.siteTitleAlt ? config.siteTitleAlt : "",
+          headline: title,
+          image: {
+            "@type": "ImageObject",
+            url: image
+          },
+          description
+        }
+      ]);
+    }
+    console.log("debug region");
+    alert("hoge");
+    console.log(schemaOrgJSONLD);
+    return (
+      <Helmet>
+        {/* General tags */}
+        <meta name="description" content={description} />
+        <meta name="image" content={image} />
+
+        {/* Schema.org tags */}
+        <script type="application/ld+json">
+          {JSON.stringify(schemaOrgJSONLD)}
+        </script>
+
+        {/* OpenGraph tags */}
+        <meta property="og:url" content={isPost ? postURL : blogURL} />
+        {isPost ? <meta property="og:type" content="article" /> : null}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta
+          property="fb:app_id"
+          content={config.siteFBAppID ? config.siteFBAppID : ""}
+        />
+
+        {/* Twitter Card tags */}
+        {/* <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:creator"
+          content={config.userTwitter ? config.userTwitter : ""}
+        />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} /> */}
+      </Helmet>
+    );
+  }
+}
+
+export default SEO;
