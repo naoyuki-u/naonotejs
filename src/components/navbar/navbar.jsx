9e8b5ce78e22b5b32ea6e50c41ad52d6cf8ba015
naoyuki29 <58511848+naoyuki29@users.noreply.github.com>
2019-12-09 10:55:17 +0900

    Change collcting method to graphQL for the categories displayed in navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
+
 // import styled from "styled-components"
 
 // import AppBar from '@material-ui/core/AppBar'
@@ -14,36 +15,103 @@ import { Row, Col } from 'antd';
 import styled from "./navbar.module.css"
 import "antd/dist/antd.less";
 
+import {get_func} from "../../utils/category_table"
+
 const config = require("../../utils/site_config");
 const {Header} = Layout;
 
 class NavBar extends Component {
   render() {
+    // alert(get_func(["satoshi", "hiroshi", "blog"]));
     return(
-    <div>
-      <Header className={styled.head_bar}>
-      <Row>
-        <Col span={2}>
-          <Link to="/">
-            <h2 className={styled.title}>{config.siteTitle}</h2>
-          </Link>
-        </Col>
-        <Col span={8} offset={14}>
-          <Menu className={styled.menuStyle} mode="horizontal" theme="dark" style={{ lineHeight: '64px' }}>
-            <Menu.Item key="1">
-              <Link to="/">BLOG</Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/about">ABOUT</Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <a href={"mailto:" + config.userEmail}>E-MAIL</a>
-            </Menu.Item>
-          </Menu>
-        </Col>
-      </Row>
-      </Header>
-    </div>
+      <StaticQuery
+        query={graphql`
+          query{
+            allMarkdownRemark{
+              edges{
+                node{
+                  frontmatter{
+                    category
+                  }
+                }
+              }
+            }
+          }
+        `}
+        render={data => {
+          const edges = data.allMarkdownRemark.edges;
+          const cate_set = new Set();
+          edges.forEach(edge => {
+            if (edge.node.frontmatter.category){
+              cate_set.add(edge.node.frontmatter.category);
+            }
+          })
+          const display_categories = get_func(Array.from(cate_set));
+
+          return(
+            <div>
+              <Header className={styled.head_bar}>
+              <Row>
+                <Col span={4}>
+                  <Link to="/">
+                    <h2 className={styled.title}>{config.siteTitle}</h2>
+                  </Link>
+                </Col>
+                <Col span={14} offset={1}>
+                  <Menu className={styled.menuStyle}
+                        mode="horizontal"
+                        theme="dark"
+                        style={{ lineHeight: '64px' }}
+                  >
+                    {
+                      display_categories.map((cate) => {
+                        return(
+                          <Menu.Item key={cate.priority}>
+                            <Link
+                              to={`/${(config.categoryPagesRoot)}` +
+                                  `/${(cate.slug)}/1`}
+                              key={cate.slug}>
+                                {cate.display}
+                              </Link>
+                          </Menu.Item>
+                        )
+                      })
+                    }
+                  </Menu>
+                </Col>
+
+                <Col span={4} offset={1} xl>
+                  <Menu className={styled.menuStyle}
+                        mode="horizontal"
+                        theme="dark"
+                        style={{ lineHeight: '64px' }}>
+                                        <Menu.Item key="about">
+                      <Link to="/about">ABOUT</Link>
+                    </Menu.Item>
+
+                    <Menu.Item key="email">
+                        <a href={"mailto:" + config.userEmail}>E-MAIL</a>
+                    </Menu.Item>
+                  </Menu>
+                </Col>
+                {/*
+                <Col span={1}>
+                  <Menu className={styled.menuStyle}
+                        mode="horizontal"
+                        theme="dark"
+                        style={{ lineHeight: '64px' }}>
+                    <Menu.Item key="email">
+                      <a href={"mailto:" + config.userEmail}>E-MAIL</a>
+                    </Menu.Item>
+                  </Menu>
+                </Col> */}
+              </Row>
+              </Header>
+            </div>
+          )
+        }}
+      />
+
     )
   }
 }
